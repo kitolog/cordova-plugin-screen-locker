7b3016fb57b8ad075196cbb2d3accbaf6f7d14ef
kit <nikita.kosilo@gmail.com>
2015-08-14 01:12:38 +0300

    Added lock method

--- www/ScreenLocker.js
+++ www/ScreenLocker.js
@@ -2,11 +2,31 @@ var exec = require('cordova/exec');
 
 var screenLocker = {
     unlock: function(successCallback, errorCallback, timeout) {
+
+        if(typeof timeout == 'undefined'){
+            timeout = 0;
+        }
         exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'ScreenLocker', // mapped to our native Java class called "ScreenLocker"
             'unlock', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "timeout": timeout
+            }]
+        );
+     },
+     lock: function(successCallback, errorCallback, timeout) {
+
+        if(typeof timeout == 'undefined'){
+            timeout = 0;
+        }
+
+        exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'ScreenLocker', // mapped to our native Java class called "ScreenLocker"
+            'lock', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "timeout": timeout
             }]
