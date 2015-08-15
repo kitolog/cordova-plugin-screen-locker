e0a39cdb7a92be6c6b816588fe014bbede7916a7
kit <nikita.kosilo@gmail.com>
2015-08-15 09:33:19 +0300

    Added new unlock method

--- src/android/ScreenLocker.java
+++ src/android/ScreenLocker.java
@@ -7,6 +7,7 @@ import org.apache.cordova.CordovaInterface;
 
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
+import android.os.PowerManager;
 
 import android.util.Log;
 import android.provider.Settings;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
+import android.app.KeyguardManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,6 +25,7 @@ import org.json.JSONObject;
 public class ScreenLocker extends CordovaPlugin {
     public static final String TAG = "ScreenLocker";
     public static final String ACTION_UNLOCK = "unlock";
+    public static final String ACTION_UNLOCK_WINDOW = "unlock_window";
     public static final String ACTION_LOCK = "lock";
 
     /**
@@ -63,9 +66,10 @@ public class ScreenLocker extends CordovaPlugin {
 //                result = true;
                 callbackContext.error("Not implemented");
                 result = false;
-            } else if (ACTION_UNLOCK.equals(action)) {
+            } else if (ACTION_UNLOCK_WINDOW.equals(action)) {
 //                Unlock
 //                http://developer.android.com/reference/android/app/Activity.html#getWindow()
+                Log.v(TAG, "ScreenLocker received ACTION_UNLOCK_WINDOW");
                 cordova.getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -73,6 +77,32 @@ public class ScreenLocker extends CordovaPlugin {
                         window.addFlags(LayoutParams.FLAG_DISMISS_KEYGUARD);
                         window.addFlags(LayoutParams.FLAG_SHOW_WHEN_LOCKED);
                         window.addFlags(LayoutParams.FLAG_TURN_SCREEN_ON);
+                        Log.v(TAG, "ScreenLocker received SUCCESS:" + action);
+                        callbackContext.success();
+                    }
+                });
+
+                result = true;
+            } else if (ACTION_UNLOCK.equals(action)) {
+//                Unlock
+//                http://developer.android.com/reference/android/app/Activity.html#getWindow()
+                Log.v(TAG, "ScreenLocker received ACTION_UNLOCK");
+                cordova.getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PowerManager powerManager = (PowerManager) cordova.getActivity().getSystemService(Context.POWER_SERVICE);
+                        PowerManager.WakeLock wakeLock = powerManager.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK
+                                | PowerManager.FULL_WAKE_LOCK
+                                | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+                        if(wakeLock.isHeld()) {
+                            wakeLock.release();
+                        }
+                        wakeLock.acquire();
+
+                        KeyguardManager keyguardManager = (KeyguardManager) cordova.getActivity().getSystemService(Context.KEYGUARD_SERVICE);
+                        KeyguardManager.KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("Tag");
+                        keyguardLock.disableKeyguard();
+
                         Log.v(TAG, "ScreenLocker received SUCCESS:" + action);
                         callbackContext.success();
                     }
--- www/ScreenLocker.js
+++ www/ScreenLocker.js
@@ -16,6 +16,21 @@ var screenLocker = {
             }]
         );
      },
+     unlockWindow: function(successCallback, errorCallback, timeout) {
+
+        if(typeof timeout == 'undefined'){
+            timeout = 0;
+        }
+        exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'ScreenLocker', // mapped to our native Java class called "ScreenLocker"
+            'unlock_window', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "timeout": timeout
+            }]
+        );
+     },
      lock: function(successCallback, errorCallback, timeout) {
 
         if(typeof timeout == 'undefined'){
