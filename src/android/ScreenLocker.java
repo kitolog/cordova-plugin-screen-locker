07c05ba4c840727f04cd1b8296c24c41e475e668
Piotr.Hauza <p.hauza@cctechnology.pl>
2016-06-01 16:01:49 +0200

    Refactoring.

--- src/android/ScreenLocker.java
+++ src/android/ScreenLocker.java
@@ -25,7 +25,6 @@ import org.json.JSONObject;
 public class ScreenLocker extends CordovaPlugin {
     public static final String TAG = "ScreenLocker";
     public static final String ACTION_UNLOCK = "unlock";
-    public static final String ACTION_UNLOCK_WINDOW = "unlock_window";
     public static final String ACTION_LOCK = "lock";
 
     /**
@@ -59,21 +58,6 @@ public class ScreenLocker extends CordovaPlugin {
                 Log.v(TAG, "Lock action not implemented, please wait for a new plugin version");
                 callbackContext.error("Not implemented");
                 result = false;
-            } else if (ACTION_UNLOCK_WINDOW.equals(action)) {
-                Log.v(TAG, "ScreenLocker received ACTION_UNLOCK_WINDOW");
-                cordova.getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Window window = cordova.getActivity().getWindow();
-                        window.addFlags(LayoutParams.FLAG_DISMISS_KEYGUARD);
-                        window.addFlags(LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-                        window.addFlags(LayoutParams.FLAG_TURN_SCREEN_ON);
-                        Log.v(TAG, "ScreenLocker received SUCCESS:" + action);
-                        callbackContext.success();
-                    }
-                });
-
-                result = true;
             } else if (ACTION_UNLOCK.equals(action)) {
                 Log.v(TAG, "ScreenLocker received ACTION_UNLOCK");
                 cordova.getActivity().runOnUiThread(new Runnable() {
